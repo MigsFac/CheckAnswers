1e93c8e051f78acf8c5f745bffbd6393c8543c76
MigsFac <migsfactory@gmail.com>
2024-11-16 18:22:08 +0900

    Update db.py

--- db.py
+++ db.py
@@ -5,11 +5,12 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_DIR = os.path.join(BASE_DIR, "database")
 
 IntegrationDB = os.path.join(DATABASE_DIR, "IntegrationDB.db")
-DemoDB = os.path.join(DATABASE_DIR, "DemoDB.db")
+
 # if os.path.exists("IntegrationDB.db"):
 #    os.remove("IntegrationDB.db")
 
 
+# メインDB
 def create_Title():
     con = sqlite3.connect(IntegrationDB)
     cur = con.cursor()
@@ -23,17 +24,22 @@ def create_Title():
                 );
     """
     )
-    cur.execute("SELECT COUNT(*) FROM users")
-    if cur.fetchone()[0] == 0:
-        password = "admin"
-        hashed_pass = generate_password_hash(password)
-        cur.execute(
-            """
-                    INSERT INTO users(username,password,role)
-                    VALUES(?,?,?)
-                    """,
-            ("admin", hashed_pass, "admin"),
-        )
+    try:
+        cur.execute("SELECT COUNT(*) FROM users WHERE username = ?", ("admin",))
+        if cur.fetchone()[0] == 0:
+            password = "admin"
+            hashed_pass = generate_password_hash(password)
+            cur.execute(
+                """
+                        INSERT INTO users(username,password,role)
+                        VALUES(?,?,?)
+                        """,
+                ("admin", hashed_pass, "admin"),
+            )
+            con.commit()
+    except:
+        print("Error m:", e)
+
     con.execute(
         """
         CREATE TABLE IF NOT EXISTS BookTitle(
@@ -64,14 +70,11 @@ def create_Title():
                 );
                 """
     )
-    con.commit()
-    con.close()
 
-    con = sqlite3.connect(DemoDB)
-    cur = con.cursor()
+    # DemoDB用
     con.execute(
         """
-        CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS DEMOusers (
                 user_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
                 username TEXT NOT NULL UNIQUE,
                 password TEXT NOT NULL,
@@ -79,20 +82,26 @@ def create_Title():
                 );
     """
     )
-    cur.execute("SELECT COUNT(*) FROM users")
-    if cur.fetchone()[0] == 0:
-        password = "admin"
-        hashed_pass = generate_password_hash(password)
-        cur.execute(
-            """
-                    INSERT INTO users(username,password,role)
+
+    try:
+        cur.execute("SELECT COUNT(*) FROM DEMOusers WHERE username = ?", ("admin",))
+        if cur.fetchone()[0] == 0:
+            password = "admin"
+            hashed_pass = generate_password_hash(password)
+            cur.execute(
+                """
+                    INSERT INTO DEMOusers(username,password,role)
                     VALUES(?,?,?)
                     """,
-            ("admin", hashed_pass, "admin"),
-        )
+                ("admin", hashed_pass, "admin"),
+            )
+            con.commit()
+    except Exception as e:
+        print("Error d:", e)
+
     con.execute(
         """
-        CREATE TABLE IF NOT EXISTS BookTitle(
+        CREATE TABLE IF NOT EXISTS DEMOBookTitle(
                 book_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
                 user_id INTEGER,
                 Title TEXT,
@@ -104,7 +113,7 @@ def create_Title():
     )
     con.execute(
         """
-        CREATE TABLE IF NOT EXISTS ResultList(
+        CREATE TABLE IF NOT EXISTS DEMOResultList(
                 result_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
                 book_id INTEGER,
                 user_id INTEGER,
@@ -119,6 +128,61 @@ def create_Title():
                 FOREIGN KEY (user_id) REFERENCES users (user_id)
                 );
                 """
+    )
+
+    # glossaryDB
+    con.row_factory = sqlite3.Row
+    con.execute(
+        """
+        CREATE TABLE IF NOT EXISTS glossary (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            reading TEXT,
+            description TEXT UNIQUE
+        );
+    """
+    )
+
+    con.execute(
+        """
+        CREATE TABLE IF NOT EXISTS tags(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tag TEXT UNIQUE
+        );
+    """
+    )
+
+    con.execute(
+        """
+        CREATE TABLE IF NOT EXISTS LargeTags(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            LargeTag TEXT UNIQUE
+        );
+    """
+    )
+
+    con.execute(
+        """
+        CREATE TABLE IF NOT EXISTS glossary_tags(
+            glossary_id INTEGER,
+            tag_id INTEGER,
+            FOREIGN KEY (glossary_id) REFERENCES glossary (id),
+            FOREIGN KEY (tag_id) REFERENCES tags (id),
+            PRIMARY KEY (glossary_id, tag_id)
+        );
+    """
+    )
+
+    con.execute(
+        """
+        CREATE TABLE IF NOT EXISTS LargeTags_tags(
+            LargeTag_id INTEGER,
+            tag_id INTEGER,
+            FOREIGN KEY (LargeTag_id) REFERENCES LargeTags (id),
+            FOREIGN KEY (tag_id) REFERENCES tags (id),
+            PRIMARY KEY (LargeTag_id, tag_id)
+        );
+    """
     )
     con.commit()
     con.close()
