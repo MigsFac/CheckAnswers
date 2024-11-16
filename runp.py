2f35eb1ec4e28834f92963851db81d0824ae0e9b
MigsFac <migsfactory@gmail.com>
2024-11-16 21:21:24 +0900

    Update runp.py

--- runp.py
+++ runp.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from views import app
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 # import redis
 # app = Flask(__name__)
 app.config.from_object("config")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+DBSQL = SQLAlchemy(app)
 
 # app.config['SESSION_TYPE'] = 'redis'
 # app.config['SESSION_PERMANENT'] = False
@@ -11,6 +18,7 @@ app.config.from_object("config")
 # app.config['SESSION_KEY_PREFIX'] = 'flask_session:'
 # app.config['SESSION_REDIS'] = redis.Redis(host='localhost',port=6379,db=0)
 # Session(app)
+
 
 import db
 
